b46e2253d0f3a50ef92c1c869ea4c8266d3d674c
AsherTechs <Asherbasco92@gmail.com>
2025-02-27 22:45:40 +0800

    Added Security, Reset Password

--- src/MainSceneController.java
+++ src/MainSceneController.java
@@ -1,15 +1,34 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
-public class MainSceneController {
+public class MainSceneController implements Initializable {
+    @FXML
+    private ComboBox<String> Security_Box;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String[] items = { "What was the name of your favorite childhood pet?", "What year was your grandmother born?",
+                "What is your favorite sport?", "What was your childhood nickname?" };
+        Security_Box.getItems().addAll(items);
+    }
+
+    @FXML
+    private Label Already_Have;
 
     @FXML
     private Pane BackgroundPane;
@@ -21,10 +40,28 @@ public class MainSceneController {
     private Label Confirm_Password;
 
     @FXML
+    private Label Dont_Have;
+
+    @FXML
     private Label Enter_Password;
 
     @FXML
     private Label Enter_User;
+
+    @FXML
+    private BorderPane Reset_Frame;
+
+    @FXML
+    private AnchorPane Forget_Background;
+
+    @FXML
+    private Label Forget_Text;
+
+    @FXML
+    private AnchorPane Forget_Top;
+
+    @FXML
+    private Hyperlink GoBack_Link;
 
     @FXML
     private AnchorPane Left_Frame;
@@ -39,7 +76,10 @@ public class MainSceneController {
     private Label Login_Text;
 
     @FXML
-    private ImageView Logo;
+    private ImageView Logo_Forget;
+
+    @FXML
+    private ImageView Logo_Register;
 
     @FXML
     private ImageView Logo_Text;
@@ -49,6 +89,9 @@ public class MainSceneController {
 
     @FXML
     private TextField PH_User;
+
+    @FXML
+    private Label Pass_Reset;
 
     @FXML
     private Button Register_Btn;
@@ -69,16 +112,95 @@ public class MainSceneController {
     private AnchorPane Registration_Bg;
 
     @FXML
+    private Hyperlink Registration_Link;
+
+    @FXML
     private AnchorPane Registration_Top;
+
+    @FXML
+    private Hyperlink Reset_Link;
+
+    @FXML
+    private Button Reset_Btn;
+
+    @FXML
+    private TextField Reset_PassField;
+
+    @FXML
+    private TextField Reset_UserField;
 
     @FXML
     private AnchorPane Right_Frame;
 
     @FXML
-    private ComboBox<?> Security_Box;
+    private TextField Security_Answer1;
+
+    @FXML
+    private TextField Security_Answer2;
 
     @FXML
     private Label Security_Question;
+
+    @FXML
+    private Label Security_Question1;
+
+    @FXML
+    private Label User_Reset;
+
+    @FXML
+    void Answer_Box1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Answer_Box2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Click_Here(ActionEvent event) {
+
+        TranslateTransition slideTaskPaneIn = new TranslateTransition(Duration.seconds(0.5), Register_Frame);
+        slideTaskPaneIn.setToX(-1225);
+        slideTaskPaneIn.play();
+
+        // Slide DefaultMenu from right to left
+        TranslateTransition slideDefaultMenuOut = new TranslateTransition(Duration.seconds(0.5), Login_Frame);
+        slideDefaultMenuOut.setToX(1225);
+        slideDefaultMenuOut.play();
+
+    }
+
+    @FXML
+    void GoBack_Btn(ActionEvent event) {
+
+        TranslateTransition slideTaskPaneIn = new TranslateTransition(Duration.seconds(0.5), Login_Frame);
+        slideTaskPaneIn.setToX(-10); // Move TaskPane to original position
+        slideTaskPaneIn.play();
+
+        // Slide DefaultMenu from right to left
+        TranslateTransition slideDefaultMenuOut = new TranslateTransition(Duration.seconds(0.5), Register_Frame);
+        slideDefaultMenuOut.setToX(10); // Move DefaultMenu off-screen to the left
+        slideDefaultMenuOut.play();
+
+    }
+
+    @FXML
+    void Reset(ActionEvent event) {
+
+        TranslateTransition slideTaskPaneIn = new TranslateTransition(Duration.seconds(0.5), Reset_Frame);
+        slideTaskPaneIn.setToX(-2607);
+        slideTaskPaneIn.play();
+
+        TranslateTransition slideRegisterFrameOut = new TranslateTransition(Duration.seconds(0.5), Register_Frame);
+        slideRegisterFrameOut.setToX(1000);
+        slideRegisterFrameOut.play();
+
+        TranslateTransition slideLoginFrameOut = new TranslateTransition(Duration.seconds(0.5), Login_Frame);
+        slideLoginFrameOut.setToX(1000);
+        slideLoginFrameOut.play();
+
+    }
 
     @FXML
     void Login_Click(ActionEvent event) {
@@ -112,6 +234,21 @@ public class MainSceneController {
 
     @FXML
     void Register_UsernameBox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Reset_Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Reset_Password(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Reset_Username(ActionEvent event) {
 
     }
 
